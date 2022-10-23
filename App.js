441099d504809f313459c78007088284a5c26721
dongjay00 <cjswodlehdgn@gmail.com>
2022-10-23 10:01:15 +0900

    feat: bottom view and empty view

--- App.js
+++ App.js
@@ -2,19 +2,28 @@ import React from 'react';
 import {StyleSheet} from 'react-native';
 import DateHead from './src/components/DateHead';
 import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
+import AddTodo from './src/components/AddTodo';
+import Empty from './src/components/Empty';
 
 function App() {
   const today = new Date();
 
   return (
     <SafeAreaProvider>
-      <SafeAreaView edges={['bottom']}>
+      <SafeAreaView edges={['bottom']} style={styles.block}>
         <DateHead date={today} />
+        <Empty />
+        <AddTodo />
       </SafeAreaView>
     </SafeAreaProvider>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  block: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+});
 
 export default App;
